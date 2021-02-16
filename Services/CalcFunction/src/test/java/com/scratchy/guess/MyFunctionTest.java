d1e97842363e42e78af37b700b00480a286a67e4
sic\s003412 <david.ward@standard.com>
2021-02-16 14:02:57 -0800

    Migrated Calc to a serverless function

---
+++ Services/CalcFunction/src/test/java/com/scratchy/guess/MyFunctionTest.java
@@ -0,0 +1,30 @@
+package com.scratchy.guess;
+
+import com.microsoft.azure.functions.*;
+import com.scratchy.guess.service.GuessService;
+import java.util.*;
+import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.*;
+
+class MyFunctionTest {
+
+    @Test
+    public void testHttpTriggerJava() throws Exception {
+//        @SuppressWarnings("unchecked")
+//        final HttpRequestMessage<Optional<String>> req = mock(HttpRequestMessage.class);
+//        final GuessService guessService = mock(GuessService.class);
+//
+//        final Map<String, Integer> queryParams = new HashMap<>();
+//        queryParams.put("actualValue", 123);
+//
+//
+//
+//        doReturn(queryParams).when(req).getQueryParameters();
+//
+//
+//
+//
+//
+
+    }
+}
